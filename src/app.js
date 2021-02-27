dfb998060b632ca18bb84f7c6fb864fa0ab637fe
Tyler Orndorff <orndorfftyler801@gmail.com>
2021-02-27 11:37:19 -0500

    created migration scripts

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const authRouter = require('./auth/auth-router')
 
 const app = express()
 
@@ -13,11 +14,19 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: CLIENT_ORIGIN
+    })
+)
+app.use('/api/auth', authRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, boilerplate!')
 })
+
+app.get('/api/*', (req, res) => {
+    res.json({ok: true});
+  });
 
 app.use(function errorHandler(error, req, res, next) {
     let response
--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
-const app = require('./app')
+const { PORT, DATABASE_URL } = require('./config')
 
-const { PORT } = require('./config')
+const app = require('./app')
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL+'?ssl=true'
+})
+
+app.set('db', db)
+  
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Server listening at ${PORT}`)
 })
