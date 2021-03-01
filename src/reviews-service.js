9793322573d428291d0fbaffdf93a0f765d1afd9
Tyler Orndorff <orndorfftyler801@gmail.com>
2021-03-01 14:51:15 -0500

    get username troubleshooting 4

--- src/reviews-service.js
+++ src/reviews-service.js
@@ -1,10 +1,27 @@
 const ReviewsService = {
     getAllReviewsPerBook(knex, book_id) {
         let bookReviews = knex.from('reviews').select('*').where('book_id', book_id);
+
+        /*
         for (let i = 0; i < bookReviews.length; i++) {
             bookReviews[i]['username'] = 'test';//knex.from('users').select('username').where('id', bookReviews[i]['user_id']).first();
         }
-        return bookReviews;
+        */
+       let outArr = [];
+       for (let i = 0; i < bookReviews.length; i++ ){
+         let temp = {};
+         temp.reviewId = bookReviews[i]['review_id'];
+         temp.bookId = bookReviews[i]['book_id'];
+         temp.title = bookReviews[i]['title'];
+         temp.contents = bookReviews[i]['contents'];
+         temp.helpCount = bookReviews[i]['help_count'];
+         temp.user = bookReviews[i]['user_id'];
+         outArr.push(temp);
+       }
+     
+
+
+        return outArr;
     },
     insertReview(knex, newRev) {
         let newId = knex.from('users').select('id').where('username', newRev.user).first()//.then(rows => {return rows[0]});
