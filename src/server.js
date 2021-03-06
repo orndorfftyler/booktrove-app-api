2ac690acd606cdb2826bc69db81a9a40efad3962
Tyler Orndorff <orndorfftyler801@gmail.com>
2021-03-06 15:42:38 -0500

    ssl: { rejectUnauthorized: false }

--- src/server.js
+++ src/server.js
@@ -3,10 +3,21 @@ const { PORT, DATABASE_URL } = require('./config')
 const app = require('./app')
 const knex = require('knex')
 
+/*
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL
 })
+*/
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  }
+})
+
 
 app.set('db', db)
   
