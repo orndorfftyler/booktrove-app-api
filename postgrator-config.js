c01e0fff0bc02080b90d80c6397b5de0c83b21d1
Tyler Orndorff <orndorfftyler801@gmail.com>
2021-03-06 14:30:34 -0500

    fixing deploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,6 +5,6 @@ module.exports = {
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DB_URL
-    //: process.env.DATABASE_URL
-    : process.env.DATABASE_URL+'?ssl=true',
+    : process.env.DATABASE_URL
+    //: process.env.DATABASE_URL+'?ssl=true',
 }
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    //DATABASE_URL: process.env.DATABASE_URL || 'postgresql://booktrover@localhost/booktrove',
-    DATABASE_URL: process.env.DATABASE_URL+'?ssl=true' || 'postgresql://admin@localhost/book_db',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://booktrover@localhost/booktrove',
+    //DATABASE_URL: process.env.DATABASE_URL+'?ssl=true' || 'postgresql://admin@localhost/book_db',
     JWT_SECRET: process.env.JWT_SECRET || 'books-are-great',
 }
